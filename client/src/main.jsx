2112fa0614da4161856b839e5665a6ad058e821c
ismatjahangirov <cahangirov.ismet@gmail.com>
2026-05-04 15:30:54 +0400

    fix(client): vite config, routing və css xətaları düzəldildi

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
+import { BrowserRouter } from 'react-router-dom';
 import './i18n';
 import App from './App.jsx';
 import './index.css';
@@ -9,7 +10,9 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
